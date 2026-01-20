7e321764245005dbe086ac67b222a47e50817197
Mario Henrique <mariohenriquelp@gmail.com>
2026-01-20 20:03:50 -0300

    Entidades criadas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { ClientModule } from './client/client.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule, ReservationModule, ClientModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
