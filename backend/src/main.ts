ae2be88ecf55cbeb691d1fa4ad1344975bf56f0c
Mario Henrique <mariohenriquelp@gmail.com>
2026-01-20 22:19:36 -0300

    Adicionando swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "nestjs-pino";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useLogger(app.get(Logger));
+
+    const config = new DocumentBuilder()
+        .setTitle("Biblioteca API")
+        .setDescription("API de gerenciamento de biblioteca")
+        .setVersion("1.0")
+        .addTag("biblioteca")
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api/docs", app, document);
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
